cfedc041092750f4720a9a722a52ad15ddc7e250
sid14-14 <sidd.work.mail@gmail.com>
2024-08-14 09:31:53 +0530

    created sign up api route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -11,10 +12,11 @@ mongoose.connect(process.env.MONGO).then(() => {
         console.log(err);
     });
 const app = express();
-
+app.use(express.json()) //to allow to send json to server, else  we will get undefined if we send some json to server
 // req is the data we get from client side, res is data we send back from server side
 
 app.use("/backend/user", userRouter);
+app.use("/backend/auth", authRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
