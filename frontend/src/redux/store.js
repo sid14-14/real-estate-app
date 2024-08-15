c5ee267607bd1960f2f213f547d252343dd9ddda
sid14-14 <sidd.work.mail@gmail.com>
2024-08-15 10:26:13 +0530

    added redux persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,27 @@
 //redux used so we have access to user data in different components
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers,configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
+  //we combine the reducer first then we gona persist reducer
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       //this makes sure we not get err when we not serialize vars
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
