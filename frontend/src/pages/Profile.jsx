959e88ac2832189a5815187c6160344be5d7cce9
laz-cow-14 <sidd.work.mail@gmail.com>
2024-08-17 12:10:04 +0530

    crated listing page ui

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -16,6 +16,7 @@ import {
   signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { app } from '../firebase';
 
 export default function Profile() {
@@ -161,6 +162,10 @@ export default function Profile() {
         <input type="email" placeholder='email'  defaultValue={currentUser.email} onChange={handleChange} id='email' className='border p-3 rounded-lg'  />
         <input type="password" placeholder='password' onChange={handleChange} id='password' className='border p-3 rounded-lg'  />
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'> {loading ? 'Loading...' : 'Update'} </button>
+        {/* as we jst have to go to different page we crate link not button */}
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
