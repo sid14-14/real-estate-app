caa706f235e90af0eb0ba539548081851738ff65
laz-cow-14 <sidd.work.mail@gmail.com>
2024-08-18 11:03:46 +0530

    created search page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 export default function App() {
   return <BrowserRouter>
@@ -17,9 +18,10 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/search' element={<Search />} />
       <Route path='/about' element={<About />} />
       <Route path='/listing/:listingId' element={<Listing />} />
-      
+
       <Route element={<PrivateRoute />}>
         <Route path='/profile' element={<Profile />} />
         <Route path='/create-listing' element={<CreateListing />} />
