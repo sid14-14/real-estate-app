3e8f50c06a79f72c0ceaf518f0785d481101c6e1
laz-cow-14 <sidd.work.mail@gmail.com>
2024-08-18 13:58:50 +0530

    added pre-installation

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,6 +14,9 @@ mongoose.connect(process.env.MONGO).then(() => {
     .catch((err) => {
         console.log(err);
     });
+  
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json()) //to allow to send json to server, else  we will get undefined if we send some json to server
 // req is the data we get from client side, res is data we send back from server side
@@ -25,6 +29,12 @@ app.listen(3000, () => {
 app.use("/backend/user", userRouter);
 app.use("/backend/auth", authRouter);
 app.use('/backend/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => { //any path we go to except above three we run index.html
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+})
 
 //THIS IS MIDDLEWARE
 //error is the error coming from input of middleware
