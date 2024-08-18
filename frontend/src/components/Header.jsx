57e8cce38723076a0c17e44ce795c810a9ef1f86
laz-cow-14 <sidd.work.mail@gmail.com>
2024-08-18 10:51:06 +0530

    added search into the header and syncronized it with url

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,28 @@
 import { FaSearch } from 'react-icons/fa';
 //link will bring us from one page to another w/o re-loading
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search); //when we search for new term, we want to keep prev terms as it is
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => { //this keeps the search in header and in url in sync
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -15,14 +33,19 @@ export default function Header() {
           </Link>
         </h1>
         <form
+          onSubmit={handleSubmit}
           className='bg-slate-100 p-3 rounded-lg flex items-center'
         >
           <input
             type='text'
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className='text-slate-600' />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
         </form>
         {/* flex makes sure home about signin comes next to each other */}
         <ul className='flex gap-4'>
