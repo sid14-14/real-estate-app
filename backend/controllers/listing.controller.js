ec6f405a921139b157457753933148cc89a3590e
laz-cow-14 <sidd.work.mail@gmail.com>
2024-08-18 10:00:59 +0530

    created search api route

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -64,4 +64,52 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
   
-  };
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true] }; //if offer is not selected: user sees both offers and no-offers
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i' }, //searchTerm: search anywhere in the name(in the middle too), i:ignore the case of letters
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
